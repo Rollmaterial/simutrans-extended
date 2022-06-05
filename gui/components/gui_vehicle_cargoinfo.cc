80f05a8d1f8f25c0305dfff8ded570f350532040
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-06-05 23:46:58 +0900

    CHG: 積荷の最終目的地表示に座標/工場へのアクセスボタンを追加

--- gui/components/gui_vehicle_cargoinfo.cc
+++ gui/components/gui_vehicle_cargoinfo.cc
@@ -9,7 +9,7 @@
 #include "../../simcity.h"
 #include "../../simconvoi.h"
 #include "../../simcolor.h"
-#include "../../simhalt.h"
+#include "../../simfab.h" // show_info
 
 #include "../../dataobj/translator.h"
 #include "../../halthandle_t.h"
@@ -19,6 +19,95 @@
 
 #define LOADING_BAR_WIDTH 170
 #define LOADING_BAR_HEIGHT 5
+
+gui_destination_building_info_t::gui_destination_building_info_t(koord zielpos, bool yesno)
+{
+	is_freight = yesno;
+
+	set_table_layout(4,1);
+	if( const grund_t* gr = world()->lookup_kartenboden(zielpos) ) {
+		if( const gebaeude_t* const gb = gr->get_building() ) {
+			// (1) button
+			if( is_freight && gb->get_is_factory() ) {
+				fab = gb->get_fabrik();
+				button.init(button_t::imagebox_state, NULL);
+				// UI TODO: Factory symbol preferred over general freight symbol
+				button.set_image(skinverwaltung_t::open_window ? skinverwaltung_t::open_window->get_image_id(0) : skinverwaltung_t::goods->get_image_id(0));
+				button.add_listener(this);
+			}
+			else {
+				button.init(button_t::posbutton_automatic, NULL);
+				button.set_targetpos(zielpos);
+			}
+			add_component(&button);
+
+			// (2) name
+			gui_label_buf_t *lb = new_component<gui_label_buf_t>();
+			if (fab) {
+				lb->buf().append( translator::translate(fab->get_name()) );
+			}
+			else {
+				cbuffer_t dbuf;
+				gb->get_description(dbuf);
+				lb->buf().append(dbuf.get_str());
+			}
+			lb->update();
+
+			// (3) city
+			const stadt_t* city = world()->get_city(zielpos);
+			if (city) {
+				new_component<gui_image_t>(skinverwaltung_t::intown->get_image_id(0), 0, ALIGN_CENTER_V, true);
+			}
+			else {
+				new_component<gui_empty_t>();
+			}
+
+			// (4) pos
+			lb = new_component<gui_label_buf_t>();
+
+			if (city) {
+				lb->buf().append(city->get_name());
+			}
+			lb->buf().printf(" %s ", zielpos.get_fullstr());
+			lb->update();
+		}
+		else {
+			// Maybe the building was demolished.
+			new_component<gui_label_t>("Unknown destination");
+		}
+	}
+	else {
+		// Terrain or map size changed or error
+		new_component<gui_label_t>("Invalid coordinate");
+	}
+
+	set_size(get_min_size());
+}
+
+bool gui_destination_building_info_t::action_triggered( gui_action_creator_t *comp, value_t )
+{
+	if( comp==&button && is_freight ) {
+		if( world()->access_fab_list().is_contained(fab) ) {
+			// open fab info
+			fab->show_info();
+		}
+		else {
+			remove_all();
+			is_freight = false;
+			new_component<gui_label_t>("Unknown destination"); 	// Maybe the factory was closed.
+		}
+		return true;
+	}
+	return false;
+}
+
+void gui_destination_building_info_t::draw(scr_coord offset)
+{
+	if( is_freight && fab ) {
+		button.pressed = win_get_magic((ptrdiff_t)fab);
+	}
+	gui_aligned_container_t::draw(offset);
+}
 
 gui_capacity_occupancy_bar_t::gui_capacity_occupancy_bar_t(vehicle_t *v, uint8 ac)
 {
@@ -375,7 +464,7 @@ void gui_vehicle_cargo_info_t::update()
 												// 4. destination halt
 												if(  show_loaded_detail==by_final_destination ||
 													(show_loaded_detail==by_destination_halt  &&  w.get_ziel()!=w.get_zwischenziel()) ){
-													add_table( show_loaded_detail==by_destination_halt ? 4:7, 1);
+													add_table( show_loaded_detail==by_destination_halt ? 4:5, 1);
 													{
 														// final destination building
 														if (show_loaded_detail == by_final_destination) {
@@ -390,40 +479,7 @@ void gui_vehicle_cargo_info_t::update()
 															else {
 																new_component<gui_image_t>(skinverwaltung_t::goods->get_image_id(0), 0, ALIGN_CENTER_V, true);
 															}
-															// destination building
-															cbuffer_t dbuf;
-															if (const grund_t* gr = world()->lookup_kartenboden(w.get_zielpos())) {
-																if (const gebaeude_t* const gb = gr->get_building()) {
-																	gb->get_description(dbuf);
-																}
-																else {
-																	dbuf.append(translator::translate("Unknown destination"));
-																}
-																lb = new_component<gui_label_buf_t>(SYSCOL_TEXT, gui_label_t::left);
-																lb->buf().append(dbuf.get_str());
-																lb->update();
-																lb->set_fixed_width(lb->get_min_size().w+D_H_SPACE);
-
-																const stadt_t* city = world()->get_city(w.get_zielpos());
-																if (city) {
-																	new_component<gui_image_t>(skinverwaltung_t::intown->get_image_id(0), 0, ALIGN_CENTER_V, true);
-																}
-																else {
-																	new_component<gui_empty_t>();
-																}
-
-																// pos
-																lb = new_component<gui_label_buf_t>(SYSCOL_TEXT, gui_label_t::left);
-
-																if (city) {
-																	lb->buf().append( city->get_name() );
-																}
-																lb->buf().printf(" %s ", w.get_zielpos().get_fullstr());
-																lb->update();
-															}
-															else {
-																new_component_span<gui_label_t>("Unknown destination", 3);
-															}
+															new_component<gui_destination_building_info_t>(w.get_zielpos(), w.is_freight());
 														}
 
 														// via halt(=w.get_ziel())
